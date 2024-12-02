35ac556d34cb3618678c8053ac480c7c97267c34
vJhonatan <jhonatan99653@gmail.com>
2024-12-02 14:56:17 -0300

    feat: add RentalModel

--- Models/RentalModel.cs
+++ Models/RentalModel.cs
@@ -2,5 +2,14 @@
 {
     public class RentalModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int IdBook { get; set; }
+
+        public DateTime RentalDate { get; set; }
     }
 }
