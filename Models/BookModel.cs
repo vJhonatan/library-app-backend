61c8d1ec00159dcabe7f354b437df65dc2f90744
vJhonatan <jhonatan99653@gmail.com>
2024-12-02 14:53:28 -0300

    feat: add BookModel

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -2,5 +2,14 @@
 {
     public class BookModel
     {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? AuthorName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
